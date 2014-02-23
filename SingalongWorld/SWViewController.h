3f5a95d83bb6f91d2efdd955ad0389d9d1335906
Haruka Kataoka <hrk8@drumsoft.com>
2014-02-23 09:26:14 +0900

    fix useless Geocoding request. fix wrong direction calculation. fix z-index problem. remove photos when new query requested.

--- SingalongWorld/SWViewController.h
+++ SingalongWorld/SWViewController.h
@@ -4,7 +4,22 @@
 //
 //  Created by 片岡 ハルカ on 2014/02/22.
 //  Copyright (c) 2014年 drumsoft. All rights reserved.
-//
+/*
+ This file is part of Singalong World.
+ 
+ Singalong World is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Singalong World is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Singalong World.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import <UIKit/UIKit.h>
 
